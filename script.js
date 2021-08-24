053163ba61eb79d496f7def5a1ebf8580e4ff864
Dave oyangorin <chocobar12318@gmail.com>
2021-08-24 17:28:15 +0800

    S12-a1

---
+++ script.js
@@ -0,0 +1,22 @@
+
+function output(x){
+	alert("name:" + x);
+}
+
+output("Tony");
+output("Starks");
+
+function email(x){
+	alert("email:" + x);
+}
+
+email("Ironman@gmail.com");
+
+function number(x){
+	alert("Number:" + x);
+}
+
+number("091711111");
+
+
+
